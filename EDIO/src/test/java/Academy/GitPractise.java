4563d2999790e514459022f40c83bf8e7f635cab
Mahesh <maheshnair192@gmail.com>
2020-05-27 15:36:55 +0530

    Changes in the Practise file

--- EDIO/src/test/java/Academy/GitPractise.java
+++ EDIO/src/test/java/Academy/GitPractise.java
@@ -6,5 +6,11 @@ public class GitPractise {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a ddemo class");
+		System.out.println("This is a ddemo class2");
+		System.out.println("This is a ddemo class3");
+		System.out.println("This is a ddemo class4");
+		System.out.println("This is a ddemo class6");
+		System.out.println("This is a ddemo class5");
+		
 	}
 }
